e462d0d9373847ad3c73d785360f6972780ca220
Abdullah Gazi Tapan <gazitapan@outlook.com>
2022-06-07 12:59:16 +0300

    C# ile dil bilen ogrenci sayisini ekrana yazdiran program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace dilBilenOgr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ogrSayi = 5;
+            int ingilizceBilenler = 0;
+            int almancaBilenler = 0;
+            int fransızcaBilenler = 0;
+            Console.WriteLine("İngilizce için 1");
+            Console.WriteLine("Almanca için 2");
+            Console.WriteLine("Fransızca için 3");
+            for (int i = 0; i < ogrSayi; i++)
+            {
+                Console.WriteLine($"{i + 1}. Öğrenci hangi yabancı dil dersini almaktadır:");
+                string ders = Console.ReadLine().ToLowerInvariant();
+                if (ders == "1")
+                    ingilizceBilenler++;
+                else if (ders == "2")
+                    almancaBilenler++;
+                else if (ders == "3")
+                    fransızcaBilenler++;
+            }
+            Console.Clear();
+            Console.WriteLine($"İngilizce Bilen Sayısı = {ingilizceBilenler}");
+            Console.WriteLine($"Almanca Bilen Sayısı = {almancaBilenler}");
+            Console.WriteLine($"Fransızca Bilen Sayısı = {fransızcaBilenler}");
+            Console.ReadKey();
+        }
+    }
+}
